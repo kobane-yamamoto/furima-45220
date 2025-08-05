300974f57878ba257cc8cb4ae33c1e3d5a0bf486
kobane-yamamoto <08063442768t@gmail.com>
2025-08-05 15:14:22 +0900

    コード修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
   has_many :orders
 
   # ニックネーム必須
